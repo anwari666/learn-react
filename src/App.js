45e048604d52d47c822a1fb13d4855ba4d69be47
Anwari Ilman <anwari.ilman@gmail.com>
2018-11-19 23:05:23 +0000

    first commit, follows tutorial at hackernoon

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Greetings from './Greetings';
 import './App.css';
 
-class App extends Component {
-  render() {
+
+class SimpleForm extends Component {
+  state = {
+    firstName : "...",
+    firstNameError : "",
+  };
+
+  validateName = ( name ) => {
+    const regex = /[A-Za-z]{3,}/;
+
+    return !regex.test(name)
+      ? "The name must contain at least three letters. Numbers and special characters are not allowed."
+      : "";
+  };
+
+  onFirstNameBlur = () => {
+    const { firstName } = this.state;
+
+    const firstNameError = this.validateName( firstName );
+
+    return this.setState({ firstNameError });
+  }
+
+  onFirstNameChange = ( event ) => 
+    this.setState({
+      firstName : event.target.value
+    });
+
+  render(){
+    const { firstName, firstNameError } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <label> 
+          First Name:
+          <input 
+            type="text" 
+            name="firstName" 
+            onChange={ this.onFirstNameChange } 
+            onBlur={ this.onFirstNameBlur } />
+
+          { firstNameError && <div>{ firstNameError }</div> }
+         </label>
+        <Greetings firstName={ firstName } />
+
       </div>
-    );
+    )
   }
 }
+
+// turned this into functional component.
+const App = () =>(
+  <div>
+    <SimpleForm />
+  </div>
+);
 
 export default App;
