8bf74f0386757d76c1a668beef74c965136a88f2
Anwari Ilman <anwari.ilman@gmail.com>
2018-11-28 22:28:15 +0000

    fourth exercise: todolist using redux. just created actions.js

---
+++ src/004-redux-todolist/actions.js
@@ -0,0 +1,39 @@
+// ACTION must consists of `type`, and other properties
+// it should be minimal
+// maybe consult Flux Standard Actions https://github.com/acdlite/flux-standard-action
+
+export const ADD_TODO              = 'ADD_TODO';
+export const TOGGLE_TODO           = 'TOGGLE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
+export const VisibilityFilter = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+
+// ACTION CREATORS are just pure function that produces (returns) ACTION.
+// they are called by DISPATCHERS later like this:
+// `dispatch( addTodo(text) )`
+
+export function addTodo(text){
+  return { 
+    type: ADD_TODO, 
+    text 
+  }
+}
+
+export function toggleTodo(index){
+  return { 
+    type: TOGGLE_TODO, 
+    index 
+  }
+}
+
+export function setVisibilityFilter(filter) {
+  return {
+    type: SET_VISIBILITY_FILTER,
+    filter
+  }
+}
