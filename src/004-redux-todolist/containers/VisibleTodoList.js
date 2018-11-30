f3dbe464850875674c83491a508000e1806f810b
Anwari Ilman <anwari.ilman@gmail.com>
2018-11-30 23:35:44 +0000

    fix formatting

--- src/004-redux-todolist/containers/VisibleTodoList.js
+++ src/004-redux-todolist/containers/VisibleTodoList.js
@@ -9,9 +9,9 @@ const mapStateToProps = (state) => ({
 const getVisibleTodos = (todos, filter) => {
     switch( filter ){
         case 'SHOW_COMPLETED':
-        return todos.filter( (todo) => todo.completed )
+            return todos.filter( (todo) => todo.completed )
         case 'SHOW_ACTIVE':
-        return todos.filter( (todo) => !todo.completed )
+            return todos.filter( (todo) => !todo.completed )
         case 'SHOW_ALL':
         default:
             return todos
